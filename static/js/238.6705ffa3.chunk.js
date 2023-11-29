3ef465f09783128bd98c442df7e28fdfa5bcadbf
fxxwol <90700503+fxxwol@users.noreply.github.com>
2023-11-29 01:58:48 +0000

    Deploying to gh-pages from @ fxxwol/tours-client@118c5f4e50788ab6d9d27bb4ae877159c6e50258 🚀

---
+++ static/js/238.6705ffa3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[238],{5238:function(e,t,r){r.r(t);r(2791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"ToursPage"})}}}]);
+//# sourceMappingURL=238.6705ffa3.chunk.js.map
